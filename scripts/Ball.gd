7ae9ea1022f41d3f631a26fa7e1a2aa1bfc4901d
Vinnie Pauley <vinniepauley@gmail.com>
2023-02-25 12:39:15 -0600

    basic ball reflection for walls and paddles

--- scripts/Ball.gd
+++ scripts/Ball.gd
@@ -1,16 +1,46 @@
 extends Area2D
 
-onready var CollisionShape = $CollisionShape2D
+onready var ColorRect = $ColorRect
+
+const BALL_SPEED = 400
 
 var yDirection = 1
+var xDirection = 1
+
+var screenWidth
+var ballWidth
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	connect('area_entered', self, '_on_area_entered')
+	screenWidth = get_viewport_rect().size.x
+	ballWidth = ColorRect.rect_size.x
 
 func _on_area_entered(area):
 	yDirection = yDirection * -1
-#	print('area entered in ball', area)
 
-func _process(delta):
-	position.y += 150 * delta * yDirection
+func handle_x_reflection():
+	if xDirection > 0 and position.x > screenWidth - ballWidth:
+		xDirection = xDirection * -1
+		
+	if xDirection < 0 and position.x < 0:
+		xDirection = xDirection * -1
+		
+func _physics_process(delta):
+	handle_x_reflection()
+	var velocity = Vector2.ZERO
+	velocity.y += yDirection
+	velocity.x += xDirection
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * BALL_SPEED
+
+	position.y += velocity.y * delta
+	position.x += velocity.x * delta
+	
+#func _process(delta):
+#	var fps = Engine.get_frames_per_second()
+	
+	
