410f6098e679961ed7161404cb1848daee890a38
Vinnie Pauley <vinniepauley@gmail.com>
2023-02-26 15:14:00 -0600

    some clunky hard-coded bricks being generated

--- scripts/Brick.gd
+++ scripts/Brick.gd
@@ -7,15 +7,36 @@ func set_pos(newPos: Vector2):
 	position = newPos
 
 func set_dimensions(width: int, height: int):
+	print('rect width: ', width)
 	ColorRect.rect_size.x = width
 	ColorRect.rect_size.y = height
+	
+#	ColorRect.rect_size.x
+#	CollisionShape2D.position.x = position.x - width
+
+func set_collision_boundaries(width, height):
+	
+#	print('collision stuff')
+#	print(CollisionShape2D.shape.extents.x)
+#	print(CollisionShape2D.shape.extents.y)
+	CollisionShape2D.shape.extents.x = width
+	CollisionShape2D.shape.extents.y = height
+#	
+	# don't fully understand yet but this is working for the moment
+	CollisionShape2D.position.x = CollisionShape2D.position.x - (ColorRect.rect_size.x / 2 + 10)
+	
+	# the further right it goes the farther off it is...
+	
+#	print('collision extents x:')
+#	print(CollisionShape2D.shape.extents.x)
+#	CollisionShape2D.position = position
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	print('rect x: ', ColorRect.rect_size.x)
-	print('rect y: ', ColorRect.rect_size.y)
+#func _ready():
+#	print('rect x: ', ColorRect.rect_size.x)
+#	print('rect y: ', ColorRect.rect_size.y)
 #	print('collision bounds: ')
-#	print(CollisionShape2D.shape.extents)
+#	
 	# ok it's like the positioning and whatnot is centered.. causing the match
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/BrickRow.gd
+++ scripts/BrickRow.gd
@@ -7,15 +7,20 @@ func _add_brick(position: Vector2, dimensions):
 	add_child(newBrick)
 	newBrick.set_pos(position)
 	newBrick.set_dimensions(dimensions.width, dimensions.height)
+	newBrick.set_collision_boundaries(dimensions.width / 2, dimensions.height / 2)
 
 const brickData = [
-	{ 'position': Vector2(5, 50), 'dimensions': { 'width': 150, 'height': 16} },
-	{ 'position': Vector2(5, 150), 'dimensions': { 'width': 150, 'height': 16} },
-	{ 'position': Vector2(5, 300), 'dimensions': { 'width': 150, 'height': 16} }
+	{ 'position': Vector2(800, 50), 'dimensions': { 'width': 150, 'height': 16} },
+	{ 'position': Vector2(105, 150), 'dimensions': { 'width': 150, 'height': 16} },
+	{ 'position': Vector2(670, 300), 'dimensions': { 'width': 150, 'height': 16} },
+	{ 'position': Vector2(370, 405), 'dimensions': { 'width': 150, 'height': 16} },
+	{ 'position': Vector2(60, 200), 'dimensions': { 'width': 150, 'height': 16} }
 ]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print(get_viewport_rect()) # calc the directions from here if ya want
+	
 	for data in brickData:
 		_add_brick(data.position, data.dimensions)
 
