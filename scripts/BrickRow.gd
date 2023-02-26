3c6c8028b96e3e2ce4122f4dd35b73ce60a32c59
Vinnie Pauley <vinniepauley@gmail.com>
2023-02-26 12:57:02 -0600

    make dimensions part of data object

--- scripts/BrickRow.gd
+++ scripts/BrickRow.gd
@@ -7,15 +7,15 @@ func _add_brick(position: Vector2, dimensions):
 	add_child(newBrick)
 	newBrick.set_pos(position)
 	newBrick.set_dimensions(dimensions.width, dimensions.height)
-	
+
 const brickData = [
-	Vector2(5, 50),
-	Vector2(5, 150),
-	Vector2(5, 300)
+	{ 'position': Vector2(5, 50), 'dimensions': { 'width': 150, 'height': 16} },
+	{ 'position': Vector2(5, 150), 'dimensions': { 'width': 150, 'height': 16} },
+	{ 'position': Vector2(5, 300), 'dimensions': { 'width': 150, 'height': 16} }
 ]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for data in brickData:
-		_add_brick(data, { 'width': 150, 'height': 6})
+		_add_brick(data.position, data.dimensions)
 
