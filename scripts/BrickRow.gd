86b3bfd8f5abc7de465d2d69ccd6d0859636861d
Vinnie Pauley <vinniepauley@gmail.com>
2023-02-26 12:53:54 -0600

    provide brick dimensions as param

--- scripts/BrickRow.gd
+++ scripts/BrickRow.gd
@@ -2,11 +2,11 @@ extends Area2D
 
 var BrickScene = preload("res://scenes/util/Brick.tscn")
 
-func _add_brick(position: Vector2):
+func _add_brick(position: Vector2, dimensions):
 	var newBrick = BrickScene.instance()
 	add_child(newBrick)
 	newBrick.set_pos(position)
-	newBrick.set_dimensions(100, 20)
+	newBrick.set_dimensions(dimensions.width, dimensions.height)
 	
 const brickData = [
 	Vector2(5, 50),
@@ -17,5 +17,5 @@ const brickData = [
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for data in brickData:
-		_add_brick(data)
+		_add_brick(data, { 'width': 150, 'height': 6})
 
