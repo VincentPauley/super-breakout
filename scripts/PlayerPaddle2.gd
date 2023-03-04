e842ae95dcb6998105f912c3d97322dd3773d80c
Vinnie Pauley <vinniepauley@gmail.com>
2023-03-03 21:33:18 -0600

    organization

--- scripts/PlayerPaddle2.gd
+++ scripts/PlayerPaddle2.gd
@@ -2,11 +2,15 @@ extends KinematicBody2D
 
 # TODOs:
 # [X] - need to decelerate on stops
+# [ ] - acceleration needs to reset to min on direction change 
 # [ ] - contain paddle in scene
 
 const MIN_ACCELERATION := 1.0
 const MAX_ACCELERATION := 20.0
 const ACCELERATION_FACTOR := 0.75
+const BASE_SPEED := 70
+
+onready var SCREEN_WIDTH := get_viewport_rect().size.x
 
 var _last_dir = ''
 
@@ -29,7 +33,12 @@ func _adjust_acceleration(motion: bool) -> void:
 	
 	clamp(acceleration, MIN_ACCELERATION, MAX_ACCELERATION)
 
+func _modify_x_pos(velo: float) -> void:
+	var new_pos = position.x + velo * acceleration
+	position.x = new_pos
+
 func _physics_process(delta):
+	var base_velo = BASE_SPEED * delta
 	var requesting_left: bool = _requesting_left()
 	var requesting_right: bool = _requesting_right()
 	
@@ -40,15 +49,15 @@ func _physics_process(delta):
 	# handle input movement
 	if _requesting_movement:
 		if requesting_left && !requesting_right:
-			position.x -= 50 * delta * acceleration
-			_last_dir = 'left'
+			_modify_x_pos(-1 * base_velo)
+			_last_dir = 'left' # TODO: use dir as 1 || -1 to signify the change
 		if requesting_right && !requesting_left:
-			position.x += 50 * delta * acceleration
+			_modify_x_pos(base_velo)
 			_last_dir = 'right'
 
 	# handle deceleration after input
 	if !_requesting_movement && acceleration > MIN_ACCELERATION:
 		if _last_dir == 'right':
-			position.x += 50 * delta * acceleration
+			_modify_x_pos(base_velo)
 		if _last_dir == 'left':
-			position.x -= 50 * delta * acceleration
+			_modify_x_pos(-1 * base_velo)
